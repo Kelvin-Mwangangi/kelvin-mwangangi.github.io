1cfc7e56bab746ea74075ee6efe0b4abd4d198db
Kelvin M. Mwangangi <104820682+Kelvin-Mwangangi@users.noreply.github.com>
2022-05-14 22:59:52 +0300

    p

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    spaceBetween: 5,
-    slidesPerView: 1,
-    // freeMode: true,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     // direction: 'vertical',
+//     loop: true,
+//     spaceBetween: 5,
+//     slidesPerView: 1,
+//     // freeMode: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    // keyboard: {
-    //   enabled: true,
-    //   onlyInViewport: false,
-    // },
-    // mousewheel: {
-    //   invert: true,
-    // },
-    autoplay: {
-      delay: 5000,
-    },
-});
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     // keyboard: {
+//     //   enabled: true,
+//     //   onlyInViewport: false,
+//     // },
+//     // mousewheel: {
+//     //   invert: true,
+//     // },
+//     autoplay: {
+//       delay: 5000,
+//     },
+// });
 
 jQuery(document).ready(function ($) {
 
